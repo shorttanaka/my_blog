76521271322370b4d4d35ea9834d4f677ee41822
shorttanaka <shorttanaka@gmail.com>
2018-03-17 15:51:10 +0900

    Add befor_action

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,22 +1,45 @@
 class PostsController < ApplicationController
+  before_action :set_post, only: [ :show, :edit, :update, :destroy]
+
+
   def index
+    @posts = Post.all
+    @new_posts = Post.all
+    @author = Author.first
   end
 
   def show
   end
 
   def new
+    @post = Post.new
   end
 
   def create
+    @post = Post.new(post_params)
+    @post.save
+    redirect_to "/posts/#{@post.id}" # post_path(@post)でもOK
   end
 
   def edit
   end
 
   def update
+    @post.update(post_params)
+    redirect_to "/posts/#{@post.id}"
   end
 
   def destroy
+    @post.destroy
+    redirect_to "/posts"
+  end
+
+private
+  def post_params
+      params.require(:post).permit(:title, :body, :category)
+  end
+
+  def set_post
+    @post = Post.find(params[:id])
   end
 end
